2f2e38c62bf0d5644adc04ef216a0ea745355a83
AbbeVass <134689455+AbbeVass@users.noreply.github.com>
2024-02-09 00:02:53 +0100

    Edited game file

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ fetch(`items/data/items-data.json`)
     })
     .catch(error => console.error('Error fetching items:', error));
 
-
+let score = 0;
 let items = {left: {}, right: {}};
 let possibleItems = []; // Only the keys
 
@@ -31,6 +31,12 @@ function setup() {
 
     // Get first item
     items.right = getItem();
+
+    // Set up scores before adding items
+    let highScore = localStorage.getItem("highScore");
+    if (!highScore) { highScore = 0; }
+    document.getElementById("score").innerHTML = "Score: "+score;
+    document.getElementById("highScore").innerHTML = "High Score: "+highScore;
 
     changeItems();
 }
@@ -51,25 +57,73 @@ function changeItems() {
         items.right = getItem();
     }
 
+    // Get elements
     const LeftDisplay = document.querySelector(".left");
     const RightDisplay = document.querySelector(".right");
     const LeftItem = items.left;
     const RightItem = items.right;
 
+    // Left item
     LeftDisplay.querySelector(".item-img").src = `items/images/${LeftItem.type}/${LeftItem.id}.webp`;
     LeftDisplay.querySelector(".item-type").innerHTML = LeftItem.type;
     LeftDisplay.querySelector(".item-skin").innerHTML = LeftItem.skin;
     LeftDisplay.querySelector(".item-price").innerHTML = getPrice(LeftItem.price);
 
+    // Right item
     RightDisplay.querySelector(".item-img").src = `items/images/${RightItem.type}/${RightItem.id}.webp`;
     RightDisplay.querySelector(".item-type").innerHTML = RightItem.type;
     RightDisplay.querySelector(".item-skin").innerHTML = RightItem.skin;
 }
 
 function guess(higher) {
+    const Box = document.querySelector(".btn-container");
+
+    // Save height
+    const BoxHeight = Box.offsetHeight; 
+
+    // Remove buttons
+    for (let btn of Box.querySelectorAll("button")) {
+        btn.style.display = "none";
+    }
     
+    // Show price element and adjust its margin so nothing moves
+    const Price = Box.querySelector(".item-price");
+    Price.style.display = "block";
+    Price.style.marginBottom = BoxHeight-Price.offsetHeight + "px";
+
+    // How many updates will the countup consist of
+    const Updates = 2 * 30; // seconds * fps
+    let frame = 1
+
+    // Animate the price reveal
+    function updatePrice() {
+        Price.innerHTML = getPrice(items["right"].price * ((frame) / Updates));
+        if (frame === Updates) { // Stop the countup
+            clearInterval(countUp);
+            continueGuess();
+        } else {
+            frame++;
+        }
+    }
+    let countUp = setInterval(updatePrice, 1000/30);
+
+    function continueGuess() {
+        // Check if the guess is correct
+        const LP = items["left"].price;
+        const RP = items["right"].price;
+        if ((RP >= LP && higher) || (RP <= LP && !higher)) { // The guess is correct
+            
+            // Update score
+            score++;
+            document.getElementById("score").innerHTML = "Score: "+score;
+            setTimeout(changeItems, 1000);
+        }
+    }
 }
 
+/**
+ * Rounds the price and returns it in the right currency
+ */
 function getPrice(sek) {
-    return sek + " kr";
+    return sek.toFixed(2) + " kr";
 }
