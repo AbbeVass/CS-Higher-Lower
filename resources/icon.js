eb40285f2e76d0f5730bbcb5e3e5b00c5e54a390
AbbeVass <134689455+AbbeVass@users.noreply.github.com>
2024-05-02 11:54:01 +0200

    Update icon.js

--- resources/icon.js
+++ resources/icon.js
@@ -1,6 +1,11 @@
 const IconElement = document.getElementById("icon");
 const IconsPath = "./../../resources/img/icons/";
-const Icons = ["mix.png", "gold.png", "teal.png", "white.png"];
+const Icons = [
+    "mix.png",
+    "gold.png",
+    "teal.png",
+    "white.png"
+];
 
 let icon;
 
@@ -9,10 +14,10 @@ if (document.querySelector("title").id === "index"       // If its the index-pag
 {                 
 
     const RandomNum = Math.floor(Math.random() * 100);
-    if      (RandomNum === 0) { icon = "mix.png"; }   // 1%
-    else if (RandomNum <= 10) { icon = "gold.png"; }  // 10%
-    else if (RandomNum <= 20) { icon = "teal.png"; }  // 10%
-    else                      { icon = "white.png"; } // 79%
+    if      (RandomNum === 0) { icon = Icons[0]; } // 1%
+    else if (RandomNum <= 10) { icon = Icons[1]; } // 10%
+    else if (RandomNum <= 20) { icon = Icons[2]; } // 10%
+    else                      { icon = Icons[3]; } // 79%
 
     localStorage.setItem("icon", icon);
 }
